75747e2ffc84779ec6b61f94d70e88c8f5bf0012
langhoangtien <langhoangtien@gmail.com>
2018-11-27 23:09:13 +0700

    first

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Login extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<div>
+				This is Main page
+			</div>
+			)
+	}
+}
+
+export default Login;
